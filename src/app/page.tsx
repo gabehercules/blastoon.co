09d48cfd2f0a430c1494a18563becc2c02c15ce3
Gabriel Gustavo de Hercules <gabriel.hercules@adcorp.local>
2024-07-26 13:52:38 -0300

    genesis

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="w-full h-dvh flex items-center justify-center text-yellow-500 text-3xl font-black">
+      <p>Blast Toon Co.</p>
+    </div>
+  );
+}
