0b444b31a51bdb170b3d60dae864330087d1c9b7
Gabriel <gabrielsyze@gmail.com>
2024-08-19 11:26:29 -0300

    hotfix: PREVIEW to ALPHA in logo badge

--- src/components/elements/logo/index.tsx
+++ src/components/elements/logo/index.tsx
@@ -13,7 +13,7 @@ export default function Logo() {
       />
       {/* <div className="text-lg font-rowdies">Blast Toon Co.</div> */}
       <div className="text-[10px] font-bold bg-brand-yellow text-yellow-950 px-[3px] py-[2px] rounded">
-        PREVIEW
+        ALPHA
       </div>
     </Link>
   );
