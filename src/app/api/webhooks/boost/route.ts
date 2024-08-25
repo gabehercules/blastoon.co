8f770f8471ba97aff29c9bc90db4d9b2c7d48ac9
Gabriel <gabrielsyze@gmail.com>
2024-08-25 17:17:10 -0300

    imporved webhook logs for debug and testing

--- src/app/api/webhooks/boost/route.ts
+++ src/app/api/webhooks/boost/route.ts
@@ -3,7 +3,14 @@ import { NextResponse } from "next/server";
 export async function POST(req: Request) {
   const body = await req.json();
 
-  console.log(body);
+  const { block } = body.event.data;
+  const log = block.logs[0];
+
+  const tx = log.transaction;
+
+  console.log("BLOCK", block);
+  console.log("LOG", log);
+  console.log("TRANSACTION", tx);
 
   return NextResponse.json({ message: "Hello, World!" });
 }
